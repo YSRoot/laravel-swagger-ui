0ada9cf0ab658ee6330c4f649195e98c40a03115
u.aleroev <u.aleroev@corp.mail.ru>
2021-08-19 22:12:53 +0300

    feat: add auto discovery service provider

--- src/SwaggerUIServiceProvider.php
+++ src/SwaggerUIServiceProvider.php
@@ -7,7 +7,7 @@ use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use YSRoot\SwaggerUI\Services\DocManager;
 
-class LaravelSwaggerUIServiceProvider extends PackageServiceProvider
+class SwaggerUIServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
