a094336810c0dafb98bd8ab50ba2da9abeeeae25
u.aleroev <u.aleroev@corp.mail.ru>
2021-08-22 00:22:49 +0300

    fix: gate won't authorizable

--- src/SwaggerUI.php
+++ src/SwaggerUI.php
@@ -3,6 +3,7 @@
 namespace YSRoot\SwaggerUI;
 
 use Closure;
+use Illuminate\Contracts\Auth\Access\Authorizable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
@@ -27,7 +28,7 @@ class SwaggerUI
 
     public static function gate(): Closure
     {
-        return static::$gateCallback ?? function (Request $request) {
+        return static::$gateCallback ?? function (?Authorizable $authorizable, Request $request) {
             return true;
         };
     }
